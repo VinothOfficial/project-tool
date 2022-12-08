fdfb3be5583eb41ebfcfddf2847c531257accb6d
VINOVICKY <vkofficial1710@gmail.com>
2022-12-08 13:36:31 +0530

    Welcome component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from "react";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Welcome name="vinoth" pic="https://images.pexels.com/photos/771742/pexels-photo-771742.jpeg?auto=compress&cs=tinysrgb&w=600"/>
+     <Welcome name="vicky" pic="https://images.pexels.com/photos/1704488/pexels-photo-1704488.jpeg?auto=compress&cs=tinysrgb&w=600"/>
+     <Welcome name="ganesh"pic="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"/>
+     
+      
     </div>
   );
+}
+function Counter(){
+  const [like,setlike] = useState(0);
+  const [dislike,setdislike] = useState(0);
+  return(
+    <div>
+      <button onClick={()=>setlike(like+1)}>👍 {like}</button>
+      <button onClick={()=>setdislike(dislike+1)}>👎{dislike}</button>
+    </div>
+  )
+}
+function Welcome(props) {
+  //const name = "vinoth";
+  return(
+     <div>
+      <img className="profile-pic"
+           src={props.pic}/> 
+      <h1>Hi {props.name}</h1>
+      <Counter/>
+</div>
+);
 }
 
 export default App;
